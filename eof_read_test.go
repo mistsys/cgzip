86c2dc11cd13d637e705c77c8c58bcc34b9f666f
Gurpreet Singh <gurpreetz@users.noreply.github.com>
2021-03-17 10:02:19 -0700

    goimports -w my change

--- eof_read_test.go
+++ eof_read_test.go
@@ -5,7 +5,7 @@ import (
 	"io/ioutil"
 	"testing"
 
-    "vitess.io/vitess/go/testfiles"
+	"vitess.io/vitess/go/testfiles"
 )
 
 // specialReader is a test class that will return bytes it reads from a file,
